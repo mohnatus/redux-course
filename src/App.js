630dc4c8403af9220caa37eae3c65ca1193e6004
FurryCat <furrycat.web@gmail.com>
2020-03-10 13:01:45 +0300

    add linter

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-const TestComponent = () => <p>просто render</p>
+const TestComponent = () => <p>test render</p>
 
 function App() {
   return (
